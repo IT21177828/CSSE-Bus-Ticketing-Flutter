24c9d5142e69271e4d9fc6f3dcf8ede422e76ba9
IT21180552 <it21180552@my.sliit.lk>
2023-10-13 01:33:14 +0530

    Create Folder Structure And Implement Genorate QR

---
+++ bus_ticketing_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:bus_ticketing_app/screens/qr_screen.dart';
+import 'package:bus_ticketing_app/screens/qr_reader_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Bus Ticketing System',
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: const Scaffold(
+  //       backgroundColor: Colors.white,
+  //       body: const QRScreen(),
+  //     ),
+  //   );
+  // }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(primaryColor: Colors.black54, primarySwatch: Colors.brown),
+      home:  BarcodeScannerApp(),
+    );
+  }
+}
---
+++ bus_ticketing_app/lib/screens/qr_reader_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class BarcodeScannerApp extends StatefulWidget {
+  @override
+  _BarcodeScannerAppState createState() => _BarcodeScannerAppState();
+}
+
+class _BarcodeScannerAppState extends State<BarcodeScannerApp> {
+  String _scanBarcode = "No data yet";
+
+  Future scanBarcodeNormal() async {
+    String barcodeScanRes;
+
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666", "Cancel", true, ScanMode.QR);
+    print(barcodeScanRes);
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Barcode Scanner Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: scanBarcodeNormal,
+                child: Text("Scan Barcode"),
+              ),
+              Text(
+                'Scan Result: $_scanBarcode',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
